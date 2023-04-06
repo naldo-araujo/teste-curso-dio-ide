f8e9843c13faf29c0ec7ef0fc945f5693fb81c85
naldo <reisousa19@gmail.com>
2023-04-05 22:55:14 -0300

    First commit

---
+++ src/com/dio/PrimeiroProgramaJava.java
@@ -0,0 +1,22 @@
+package com.dio;
+
+import com.dio.model.ggato;
+
+public class PrimeiroProgramaJava {
+
+	public static void main(String[] args) {
+		ggato ggato = new ggato(); 
+		livros livros = new livros();
+		System.out.println(ggato);
+		
+	    /*int a = 2;
+		int b = 3;
+			System.out.println("Hello World "+ (a+b) );*/
+	}
+
+}
+class livros {
+	private String nome;
+	private String npag;
+	
+}

6fd1365d7cb4adb8d384c48b6b60366dbe32d00b
naldo <reisousa19@gmail.com>
2023-04-07 22:48:12 -0300

    update

--- src/com/dio/PrimeiroProgramaJava.java
+++ src/com/dio/PrimeiroProgramaJava.java
@@ -6,17 +6,17 @@ public class PrimeiroProgramaJava {
 
 	public static void main(String[] args) {
 		ggato ggato = new ggato(); 
+		
 		livros livros = new livros();
 		System.out.println(ggato);
+		System.out.println(livros);
 		
-	    /*int a = 2;
-		int b = 3;
-			System.out.println("Hello World "+ (a+b) );*/
+	    
 	}
 
 }
 class livros {
-	private String nome;
-	private String npag;
+	public String nome;
+	public Integer npag;
 	
 }
